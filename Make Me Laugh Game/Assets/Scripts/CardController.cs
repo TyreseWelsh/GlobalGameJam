58c333f2762a41d5c236c4be7c441fd84cc8108f
Adam Lewis <al262506@falmouth.ac.uk>
2024-01-27 17:49:14 +0000

    Began visuals for moving cards on playing field
    Objects with the Card Controller attached can be moved around into Drop Zones.
    
    For some reason, HandController allows the player to drop cards into it but PlayAreaController does not? I added a couple of funne kyle and josh pokemon cards as placeholders to see where the cards were going and how actual card art would look with that size.

---
+++ Make Me Laugh Game/Assets/Scripts/CardController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.SetParent(transform.root);
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(CardManager.LastEnteredDropZone);
+        image.raycastTarget = true;
+    }
+}
